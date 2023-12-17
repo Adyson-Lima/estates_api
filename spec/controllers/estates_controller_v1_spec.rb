f33366a73cd7b22557df3e6ad96eadd46063de3b
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-17 02:46:49 -0300

    12° commit - tdd controller: Consegue listar um estate especifico e retornar status 200? RED

--- spec/controllers/estates_controller_v1_spec.rb
+++ spec/controllers/estates_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::EstatesController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/estates/id' do
+    it 'Consegue listar um estate especifico e retornar status 200?' do
+      get :show, params: {id: @estate.id}
+      expect(response.body).to include_json(name: 'Rio Grande do Sul')
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
