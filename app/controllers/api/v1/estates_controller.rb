ae68dfd47fd16630a21c9283e18adaf796f7ce46
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-17 02:48:28 -0300

    13° commit - tdd controller: Consegue listar um estate especifico e retornar status 200? GREEN

--- app/controllers/api/v1/estates_controller.rb
+++ app/controllers/api/v1/estates_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::EstatesController < ApplicationController
 
-  before_action :set_estate, only: %i[] # show update destroy
+  before_action :set_estate, only: %i[show] # show update destroy
 
   def index
     @estates = Estate.all 
     render json: @estates
+  end
+
+  def show
+    render json: @estate
   end
 
 private
